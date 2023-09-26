8bec75ed3ce7d9a5d04efb1a4f053dbd296cd766
mhamza-ali <hamza4841333@gmail.com>
2023-09-27 01:42:57 +0500

    till operators

--- index.js
+++ index.js
@@ -1,3 +1,96 @@
 
-console.log("Hello");
+// console.log("Hello");
+// console.log(1+2);
+
+// // variables
+// var a= 5;
+// console.log(a);
+
+// let b = "hello";
+// console.log(b);
+
+// const myName = "JS"
+// console.log(myName);
+
+// // difference between variables
+// var name = "programming";
+// var name = "coding";
+// console.log(name);
+ 
+//not possible in let to override
+// let fname = "programming";
+// let fname = "coding";
+// console.log(fname);
+
+//constant cant be changed
+// const lname = "programming";
+// const lname = "coding";
+// console.log(lname);
+
+// datatypes
+// primitive
+//  let num = 10;
+//  console.log(typeof num);
+
+//  let str = "hello";
+//  console.log(typeof str);
+
+//  let bool = false;
+//  console.log(typeof bool);
+
+//  let abc = null;
+//  console.log(typeof abc)
+
+//  let university;
+// console.log(university);
+
+//non primitive 
+
+//type conversion
+
+// let result = 3 + 'hello';
+// console.log(result);
+// console.log(typeof result)
+
+//we can solve them by explicit type conversion
+// let result = "3";
+// let result1 = Number(result)
+// console.log(result1);
+// console.log(typeof result1)
+
+// let result = 3.202;
+// let result1 = parseInt(result)
+// console.log(result1);
+// console.log(typeof result1)
+
+// operators
+// arithmetic operators
+console.log(2+5)
+console.log(2-5)
+console.log(2*5)
+console.log(2%5)
+console.log(2/5)
+console.log(2**5)
+
+// increment decrement operator
+let a= 1;
+console.log(a);
+console.log(++a); //prefix
+console.log(--a);
+console.log(a++);
+console.log(a); //postfix
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
