eb823e72db04f294982a97f93e9c699a3f8ae18d
mhamza-ali <hamza4841333@gmail.com>
2023-10-03 19:06:17 +0500

    promise ex2

--- index.js
+++ index.js
@@ -568,19 +568,26 @@
 
 // promise
 
-function fun(task){
-    return new Promise((resolve,reject)=>{
-        if(task){
-            resolve("Completed")
+// function fun(task){
+//     return new Promise((resolve,reject)=>{
+//         if(task){
+//             resolve("Completed")
     
-        }
-        else{
-            reject("Failed")
-        }
-    })
-}
+//         }
+//         else{
+//             reject("Failed")
+//         }
+//     })
+// }
 
+// let onResolve = (res)=>{
+//     console.log(res)
+// }
+// let onReject = (err)=>{
+//     console.log(err)
+// }
 
+// fun(true).then(onResolve).catch(onReject)
 
 
 
